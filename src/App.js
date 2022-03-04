c0aa871591bd045c238a18e0f910f08c7dcb84b6
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2022-03-04 17:30:36 +0500

    error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from "../src/components/Todo"
+import Todopdf from "../src/components/Pdf"
 function App() {
   return (
     <div className="App">
-      <Todo />
+      {/* <Todo /> */}
+      {/* <Pdfkit/> */}
+      <Todopdf />
     </div>
   );
 }
--- src/components/Todopdf.js
+++ src/components/Todopdf.js
@@ -4,7 +4,7 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import Input from "@mui/material/Input";
+import Tooltip from "@mui/material/Tooltip";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import Button from "@mui/material/Button";
@@ -17,8 +17,23 @@ import Stack from "@mui/material/Stack";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Pdf from "react-to-pdf";
+import PrintIcon from "@mui/icons-material/Print";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Todo = () => {
   const [charge, setCharge] = useState("");
@@ -28,6 +43,14 @@ const Todo = () => {
   const [globalIndex, setGlobalIndex] = useState("");
   const [disable, setDisable] = useState(false);
   const [updateButton, setUpdataButton] = useState(false);
+  const [disbaleBtnForPdf, setDisbaleBtnForPdf] = useState(false);
+  const [number, setRandomNumber] = useState(0);
+  //mui state
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const ref = React.createRef();
 
   function submitData(e) {
     e.preventDefault();
@@ -62,7 +85,15 @@ const Todo = () => {
     setCharge(data[e].charge);
     setAmount(data[e].amount);
   }
-
+  const options = {
+    orientation: "landscape",
+    unit: "in",
+    format: [4, 2],
+    x1: "0", //starting coords are x1 and y1
+    y1: "0",
+    x2: "2", //ending coords:
+    y2: "20",
+  };
   function updateButtonFunction() {
     setUpdataButton(false);
     data[globalIndex].charge = charge;
@@ -90,8 +121,67 @@ const Todo = () => {
     setAmount("");
     setCharge("");
   }
+  let randomNum = Math.ceil(Math.random() * 100);
+  // setRandomNumber(randomNum)
   return (
     <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <div id="changing" ref={ref}>
+              <h3 id="invioceid">{"INVOICE"}</h3>
+              {/* <p id="invoicenumber">id:{randomNum}</p> */}
+              <table id="tableWidtth">
+                <tr>
+                  <th>NAME</th>
+                  <th>PRICE</th>
+                </tr>
+                {Array.isArray(data) &&
+                  data.length > 0 &&
+                  data.map((printval, ind) => {
+                    return (
+                      <>
+                        <tr>
+                          <td>{printval.charge}</td>
+                          <td>{printval.amount}</td>
+                        </tr>
+                      </>
+                    );
+                  })}
+              </table>
+            </div>
+          </Typography>
+          <Pdf
+            targetRef={ref}
+            filename="Invoice.pdf"
+            x={50}
+            y={10}
+            x1={10}
+            scale={0.8}
+          >
+            {({ toPdf }) => (
+              <Tooltip title="Print" arrow>
+                <div id="printericonbtn">
+                  <Button
+                  variant="contained"
+                    id="printbtn"
+                    startIcon={<PrintIcon />}
+                    onClick={toPdf}
+                  >
+                    PRINT
+                  </Button>
+                </div>
+              </Tooltip>
+            )}
+          </Pdf>
+        </Box>
+      </Modal>
+
       <div id="mainContainer">
         <h1>BUDGET CALCULATOR</h1>
         <div id="inputContainer">
@@ -161,55 +251,83 @@ const Todo = () => {
               </Button>
             )}
           </div>
-
+          {/* for pdf */}
+          {/* <div id="changing" ref={ref}> */}
           {/* <"show Array data"> */}
           {Array.isArray(data) &&
             data.length > 0 &&
             data.map((val, index) => {
               return (
                 <>
-                 
                   <div id="mylist">
                     <List
+                      key={index}
                       sx={{
                         width: "100%",
-                        maxWidth: 360,
+                        //   maxWidth: 390,
                         bgcolor: "background.paper",
                         colr: "black",
                       }}
                     >
                       <ListItem
-                      key={index}
+                        key={index}
                         secondaryAction={
                           <Stack direction="row" spacing={2}>
-                            <IconButton
-                              edge="end"
-                              disabled={disable}
-                              onClick={() => {
-                                deleteitem(index);
-                              }}
-                              aria-label="delete"
-                            >
-                              <DeleteIcon />
-                            </IconButton>
-                            {/* edit button */}
-                            <IconButton
-                              edge="end"
-                              onClick={() => {
-                                editItem(index);
-                                setGlobalIndex(index);
-                              }}
-                              aria-label="edit"
-                            >
-                              <EditIcon />
-                            </IconButton>
+                            <>
+                              <Tooltip title="Edit" arrow>
+                                <IconButton
+                                  edge="end"
+                                  onClick={() => {
+                                    editItem(index);
+                                    setGlobalIndex(index);
+                                  }}
+                                  aria-label="edit"
+                                >
+                                  <EditIcon />
+                                </IconButton>
+                              </Tooltip>
+                              {/* delete btn */}
+                              <Tooltip title="Delete" arrow>
+                                <IconButton
+                                  edge="end"
+                                  disabled={disable}
+                                  onClick={() => {
+                                    deleteitem(index);
+                                  }}
+                                  aria-label="delete"
+                                >
+                                  <DeleteIcon />
+                                </IconButton>
+                              </Tooltip>
+                            </>
                           </Stack>
                         }
-                        disablePadding
+                        //   disablePadding
                       >
-                        <ListItemButton role={undefined} dense>
-                          <ListItemText primary={val.charge} />
-                          <ListItemText primary={val.amount} />
+                        <ListItemButton>
+                          <ListItemText primary={index + 1}></ListItemText>
+                          <ListItemText
+                            primary={
+                              <Typography
+                                gutterBottom
+                                variant="h6"
+                                component="h6"
+                              >
+                                {val.charge}
+                              </Typography>
+                            }
+                          />
+                          <ListItemText
+                            primary={
+                              <Typography
+                                gutterBottom
+                                variant="h6"
+                                component="h6"
+                              >
+                                ${val.amount}
+                              </Typography>
+                            }
+                          />
                         </ListItemButton>
                       </ListItem>
                     </List>
@@ -217,25 +335,30 @@ const Todo = () => {
                 </>
               );
             })}
-          {/* </div> */}
-          <div id="dltbtn">
-            {data.length > 0 ? (
+        </div>
+        <div id="dltbtn">
+          {data.length > 0 ? (
+            <Stack direction="row" spacing={1}>
               <Button
                 variant="contained"
                 onClick={deleteAllData}
                 startIcon={<DeleteIcon />}
               >
-                Clear Expenses
+                Clear
+              </Button>{" "}
+              <Button variant="contained" onClick={handleOpen}>
+                Print
               </Button>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-        <div id="totalAmount">
-          <b>{data.length > 0 ? `Total Spend$ ${totalAmount}` : ""}</b>
+            </Stack>
+          ) : (
+            ""
+          )}
         </div>
       </div>
+      <div id="totalAmount">
+        <b>{data.length > 0 ? `Total Spend $${totalAmount}` : ""}</b>
+      </div>
+      {/* </div> */}
     </>
   );
 };
